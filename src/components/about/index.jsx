493f4d52169d03eaba8f54dbbdc5a1c9cdb68749
Davi Busanello <itsme@davibusanello.net>
2020-11-01 00:00:54 -0300

    Update home page (#2)

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -23,12 +23,10 @@ const About = () => {
             <div>
               <Avatar fluid={data.placeholderImage.childImageSharp.fluid} alt="user photo" />
               <SubTitle> Front End Developer</SubTitle>
-            </div> 
+            </div>
             <div>
-              <Title> Hello, I’m AbdAli </Title>
-              <Text> I'm a digital Front End Developer hailing from <b className="text-primary lined-link">North Africa</b> living in Casablanca. </Text>
-              <Text> I love working with modern technologies, building and designing awesome projects. I prefer minimalistic & clean designs with strong user experience.</Text>
-              <Text> behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia.
+              <Title> Hello, we are UndertakeHQ</Title>
+              <Text> We deliver solutions with cutting edge technology!
               </Text>
               <ResumeButton href="resume.pdf" target="_blank"> Download resume </ResumeButton>
             </div>
